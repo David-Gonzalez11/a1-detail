3ea89c7d1d509e17822b983d699ff8335f822197
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-08-03 00:57:02 +0000

    Added process.enx to api key

--- client/components/google-maps.jsx
+++ client/components/google-maps.jsx
@@ -53,7 +53,7 @@ const position = {
 function MyComponent() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: 'AIzaSyBERMiEcRiU6VTN-RO1QTUyLnx8twkoRH0'
+    googleMapsApiKey: (process.env.API_KEY)
   });
 
   const [map, setMap] = React.useState(null);
