892f42805a31ae59b5b18127aa9bc473e2be8863
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-08-04 23:19:47 +0000

    Made delete button delete the matcbing aopointmentId

--- client/components/renderAppointments.jsx
+++ client/components/renderAppointments.jsx
@@ -8,6 +8,31 @@ export default class UserAppointments extends React.Component {
     };
     this.renderList = this.renderList.bind(this);
     this.componentDidMount = this.componentDidMount.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDelete(appointmentId) {
+    const newArray = this.state.appointments.filter(appts => {
+      return appts !== appointmentId.appointmentId;
+    });
+    console.log('appointmentIDDDD', appointmentId.appointmentId);
+    console.log(newArray);
+    const req = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Access-Token': localStorage.getItem('authToken-jwt')
+      },
+      body: JSON.stringify(this.state)
+    };
+    fetch(`/api/appointments/${appointmentId.appointmentId}`, req)
+      .then(result => {
+        console.log('reponse', result);
+        this.setState({
+          appointments: newArray,
+          isLoaded: true
+        });
+      });
   }
 
   renderList() {
@@ -35,6 +60,8 @@ export default class UserAppointments extends React.Component {
   }
 
   render() {
+    console.log('APPPS', this.state.appointments.length);
+
     const { isLoaded } = this.state;
 
     if (!isLoaded) return <span className="visually-hidden">Loading...</span>;
@@ -48,11 +75,12 @@ export default class UserAppointments extends React.Component {
 
           <div className="card text-bg-light mb-2" key={appt.appointmentId}>
             <div className="card-body">
+
+              <button onClick={this.handleDelete.bind(null, appt)} className=" text-white bg-danger">Delete</button>
               <p className="card-text text-center">What: {appt.service}</p>
               <p className="card-text text-center">When: {appt.appointmentScheduled}</p>
               <p className='card-text text-center'>Where: {appt.address}</p>
               <p className='card-text text-center'>City: {appt.city}</p>
-
             </div>
           </div>
         )}
