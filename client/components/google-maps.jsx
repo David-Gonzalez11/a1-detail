dc6054ca4b4f78eb71b6757697b9201e04d7d76c
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-10-21 04:39:34 +0000

    Completed changing layout of app

--- client/components/google-maps.jsx
+++ client/components/google-maps.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MarkerF, InfoWindowF, GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import ScheduleAppointment from './create-appointment';
 
@@ -7,15 +7,10 @@ const containerStyle = {
   height: '100vh'
 };
 
-navigator.geolocation.getCurrentPosition(function (position) {
-  console.log('Latitude is :', position.coords.latitude);
-  console.log('Longitude is :', position.coords.longitude);
-});
-
-const center = {
-  lat: 33.6846,
-  lng: -117.8265
-};
+// const center = {
+//   lat: 33.6846,
+//   lng: -117.8265
+// };
 const markerStyle = {
   position: 'absolute',
   top: '100%',
@@ -29,16 +24,26 @@ const position = {
 };
 
 function MyComponent() {
+  // const [coordss, setCoords] = useState({ lat: null, lang: null });
+  // navigator.geolocation.getCurrentPosition(function (position) {
+  //   setCoords({ lat: position.coords.latitude, lang: position.coords.longitude });
+  //   console.log('Latitude is :', position.coords.latitude);
+  //   console.log('Longitude is :', position.coords.longitude);
+  });
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: (process.env.REACT_APP_API_KEY)
 
   });
+  const center = {
+    lat: 33.6846,
+    lng: -117.8265
+  };
 
   const [map, setMap] = React.useState(null);
 
   const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(center);
+    const bounds = new window.google.maps.LatLngBounds(position);
     map.fitBounds(bounds);
     setMap(map);
   }, []);
