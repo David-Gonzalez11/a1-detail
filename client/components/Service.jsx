ca01f9b6b736ce087fe53bb9f25d512ca3a6fe4e
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2024-02-27 02:08:22 +0000

    Made change to image not appearing for exterior clean

--- client/components/Service.jsx
+++ client/components/Service.jsx
@@ -5,7 +5,7 @@ export default class Service extends React.Component {
     const exteriorClean =
  <>
       <div className="card" style={{ width: '16rem', height: '25rem' }}>
-  <img src="http://bestshinecarwash.com/wp-content/uploads/2019/07/AdobeStock_131682314-1024x683.jpeg" className="card-img-top" alt="..." />
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmMYHLPdROE-v1Tl7uKO81XekeyX06j8AdEQ&usqp=CAU" className="card-img-top" alt="..." />
   <div className="card-body">
     <h6 className="card-title">Exterior Clean</h6>
      <ul>
